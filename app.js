14f891a0229a9b902431df9179bca4bb39da6044
Phil-Code <pjdixon@hotmail.co.uk>
2023-10-23 09:27:12 +0100

    allow cross origin sharing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api.router');
+const cors = require('cors');
 const { handleSQLErrors, handleCustomErrors, handleServerErrors } = require('./controllers/error.controllers');
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', apiRouter)
